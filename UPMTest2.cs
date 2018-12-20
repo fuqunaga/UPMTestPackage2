5866ded52fbdbe18c9b8efae5abe39294409d88e
fuqunaga <fukhid@gmail.com>
2018-12-20 17:08:54 +0900

    first commit

---
+++ UPMTest2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UPMTest2 : MonoBehaviour
+{
+    public void OnGUI()
+    {
+        GUILayout.Label("UPM Test 2");
+    }
+}
